3b10d6292192cac4b767a82aabc5351fcdf3123c
Kisukeyas <kisuke.yas@gmail.com>
2022-10-25 19:39:56 +0900

    fixed

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,13 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STRAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER,
-  appId: process.env.REACT_APP_APP_ID,
+  authDomain: "line-clone-f6ad4.firebaseapp.com",
+  databaseURL: "https://line-clone-f6ad4-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "line-clone-f6ad4",
+  storageBucket: "line-clone-f6ad4.appspot.com",
+  messagingSenderId: "1060298658183",
+  appId: "1:1060298658183:web:c10a4e59923c1a2a28c322"
 };
-
-console.log(firebaseConfig);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
